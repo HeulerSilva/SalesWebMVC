19b9ebf0aff3759b68af5e50fea6ac0661c94c74
heulersilva <heulersilva@yahoo.com.br>
2026-03-25 17:18:11 -0300

    Seller details and eager loading

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -52,10 +52,22 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
         {
-           _sellerService.Remove(id);
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
 
         }
-        
-    } 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+    }
 }
