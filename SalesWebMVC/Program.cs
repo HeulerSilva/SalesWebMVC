611e61fff95498041f0b61262d7d78c57f377f7a
heulersilva <heulersilva@yahoo.com.br>
2026-03-25 11:57:33 -0300

    SellerService and basic FindAll

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
 options.UseMySQL(builder.Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));    
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
