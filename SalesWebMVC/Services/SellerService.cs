df153668ff5cf91114cef2fb0ff490facdd43e29
heulersilva <heulersilva@yahoo.com.br>
2026-03-25 18:18:21 -0300

    Update seller and custom service exception

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,6 +1,8 @@
 ﻿using SalesWebMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
+using SalesWebMVC.Services.Exceptions;
+
 namespace SalesWebMVC.Services
 {
     public class SellerService
@@ -30,5 +32,22 @@ namespace SalesWebMVC.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
