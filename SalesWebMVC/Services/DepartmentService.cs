5f3914e7b005a109c8237262c629653c0858ce4a
heulersilva <heulersilva@yahoo.com.br>
2026-03-26 15:08:58 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
@@ -10,9 +11,9 @@ namespace SalesWebMVC.Services
         {
             _context = context;
         }
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
